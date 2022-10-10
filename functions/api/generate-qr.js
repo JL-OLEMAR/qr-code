57a60e2bad34eb3244ce7f5d72489c5d2b75277e
JL-OLEMAR <joseluis19963@gmail.com>
2022-10-09 23:43:24 -0500

    fix: Fix cors requests

--- functions/api/generate-qr.js
+++ functions/api/generate-qr.js
@@ -1,16 +1,17 @@
 import qr from 'qrcode'
 
-export async function onRequestPost(context) {
-  // Contents of context object
-  // const {
-  //   request, // same as existing Worker API
-  //   env, // same as existing Worker API
-  //   params, // if filename includes [id] or [[path]]
-  //   waitUntil, // same as ctx.waitUntil in existing Worker API
-  //   next, // used for middleware or to fetch assets
-  //   data // arbitrary space for passing data between middlewares
-  // } = context
+export async function onRequestOptions() {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Max-Age': '86400'
+    }
+  })
+}
 
+export async function onRequestPost(context) {
   const { request } = context
 
   // read the body from the request
@@ -24,8 +25,13 @@ export async function onRequestPost(context) {
     }
   })
 
+  // Send all headers
   return new Response(JSON.stringify({ svg: qrImage }), {
     headers: {
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Max-Age': '86400',
       'Content-Type': 'application/json'
     }
   })
